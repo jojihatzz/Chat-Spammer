79fa8a2cdd01d4956829b41937fe80a21c036557
jojihatzz <97831328+jojihatzz@users.noreply.github.com>
2022-05-23 18:56:07 +0300

    upload

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center">Chat Spammer</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="https://github.com/jojihatzz/SchoolGradesSystemTerminalVer/blob/main/LICENSE" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+
+
+
+
+
+
+
+> Chat spammer
+
+### 🏠 [Homepage](https://github.com/jojihatzz/Chat-Spammer)
+
+## Download The [ZIP file](https://github.com/jojihatzz/Chat-Spammer/archive/refs/heads/main.zip)
+**Just run the following commands**
+```sh
+pip install -r requirements.txt
+```
+
+## Run
+
+```sh
+python spam.py
+```
+
+
+
+## Author
+
+👤 **jojihatzz**
+
+* Website: https://github.com/jojihatzz
+* Github: [@jojihatzz](https://github.com/jojihatzz)
+
+## Show your support
+
+Give a ⭐️ if this project helped you! 
+
+
+
+
+![alt text](https://github.com/jojihatzz/SchoolGradesSystemTerminalVer/blob/main/yee.jpg?raw=true)
---
+++ spam.py
@@ -0,0 +1,42 @@
+#spam message test
+import time
+import pyautogui
+import os
+
+
+def title():
+  print("""
+
+
+     _____ _           _      _____                                           
+    / ____| |         | |    / ____|                                          
+   | |    | |__   __ _| |_  | (___  _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
+   | |    | '_ \ / _` | __|  \___ \| '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+   | |____| | | | (_| | |_   ____) | |_) | (_| | | | | | | | | | | |  __/ |   
+    \_____|_| |_|\__,_|\__| |_____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
+                                   | |                                        
+                                   |_|                                        
+      made by @ghatzz
+
+  """)
+
+
+def mainapp():
+  inputf = input("Enter the message: ")
+  times = input("How many times?: ")
+  times = int(times)
+  print("Sending messages...")
+  time.sleep(2)
+  for i in range(0,times):
+    pyautogui.typewrite(inputf + '\n')
+    time.sleep(1)
+  print("Done")
+  time.sleep(2)
+  print("Quitting...")
+  quit()
+
+
+title()
+time.sleep(2)
+os.system('cls')
+mainapp()
